8ef1cc3e37415436df2f78bc5bf868c110364e1d
Punit-Yadav <punit965@gmail.com>
2022-01-06 02:09:37 +0530

    beta

--- designs.js
+++ designs.js
@@ -1,10 +1,29 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
+const gridHeight = document.querySelector('#inputHeight');
+const gridWidth = document.querySelector('#inputWidth');
+const colorChoice = document.querySelector('#colorPicker');
 
 function makeGrid() {
-
-// Your code goes here!
-
+  const mainTable = document.querySelector('#pixelCanvas');
+  mainTable.innerHTML = null;
+  if(gridHeight.value>=1 && gridHeight.value<=100 && gridWidth.value>=1 && gridWidth.value<=100) {
+    for(var i = 1; i<=gridHeight.value; i++){
+      const row = document.createElement('tr')
+      mainTable.appendChild(row)
+      for(var j = 1; j<=gridWidth.value; j++){
+        const column = document.createElement('td');
+        row.appendChild(column);
+      }
+    }
+  }
+  else {
+    alert("Please enter a number between 1 to 100");
+  }
 }
+
+function changeColor(evt){
+  if(evt.target.nodeName === "TD"){
+    evt.target.style.backgroundColor = colorChoice.value;
+  }
+}
+
+document.addEventListener('click', changeColor);

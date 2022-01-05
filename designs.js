fd64ced712914442c416dfd27c6440f301fb1707
Punit-Yadav <punit965@gmail.com>
2022-01-06 02:52:37 +0530

    fixed-variable-name-issues

--- designs.js
+++ designs.js
@@ -2,10 +2,10 @@ const gridHeight = document.querySelector('#inputHeight');
 const gridWidth = document.querySelector('#inputWidth');
 const colorChoice = document.querySelector('#colorPicker');
 // Declared three variables to store the height, width and color element.
+const mainTable = document.querySelector('#pixelCanvas');
+// mainTable stores the table element of html with id #pixelCanvas.
 
 function makeGrid() {
-    const mainTable = document.querySelector('#pixelCanvas');
-    // mainTable stores the table element of html with id #pixelCanvas.
     mainTable.innerHTML = null;
     // Making the innerHTML of mainTable null resets the grid.
     if (gridHeight.value >= 1 && gridHeight.value <= 100 && gridWidth.value >= 1 && gridWidth.value <= 100) {
@@ -33,6 +33,6 @@ function changeColor(evt) {
 // Checks if the element clicked is an <td> element.
 // Changes the background color based on the colorPicker.
 
-document.addEventListener('click', changeColor);
+mainTable.addEventListener('click', changeColor);
 // Adds an event on the table.
 // When clicked the changeColor() function is called
